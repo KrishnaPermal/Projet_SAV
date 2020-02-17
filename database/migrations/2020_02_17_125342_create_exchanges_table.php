eb55507b22523103987c4ce40b5415d3387ec1ef
KrishnaPermal <krishna.tantalidespermalnaiken@gmail.com>
2020-02-17 18:34:10 +0400

    US03 migrations tables exchanges

---
+++ database/migrations/2020_02_17_125342_create_exchanges_table.php
@@ -0,0 +1,74 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExchangesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exchanges', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('type', 50);
+            $table->string('commentaire', 50);
+
+
+         
+//on crrer des champs vides
+            $table->unsignedBigInteger('id_users');
+            $table->unsignedBigInteger('id_clients');
+            $table->unsignedBigInteger('id_exchange_types');
+
+            $table->timestamps();
+        });
+
+
+//on choisit la table dans laquelle on veut travailler
+        Schema::table('exchanges', function($table) {
+            //assign les champs vide a des foreign key
+            //id _users fait reference a l id dans la table users
+            $table->foreign('id_users')->references('id')->on('users');
+            $table->foreign('id_clients')->references('id')->on('clients');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+       
+    Schema::table('exchanges', function (Blueprint $table) {
+     
+        Schema::disableForeignKeyConstraints();
+        //supprime 
+        $table->dropIfExists('id_users');
+        $table->dropIfExists('id_clients');
+
+
+      
+
+       Schema::enableForeignKeyConstraints();
+   });
+
+  Schema::dropIfExists('exchanges');
+    }
+
+
+
+
+
+
+
+
+    
+}
